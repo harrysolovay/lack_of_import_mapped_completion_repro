b16aef045682d18ce2513a006145679bfe54f830
Harry Solovay <harrysolovay@gmail.com>
2022-12-26 14:58:35 -0800

    first commit

---
+++ index.ts
@@ -0,0 +1,5 @@
+// Intellisense works:
+import {} from "https://capi.dev/@v0.1.0-beta.22/proxy/wss:rpc.polkadot.io/@v0.9.36/mod.ts"
+
+// Intellisense does not work:
+import {} from "capi/proxy/wss:rpc.polkadot.io/@v0.9.36/mod.ts"
